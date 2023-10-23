921baf131b316dba710d08a6b7a10d9206413c54
Gitzonfire <alexatz@free.fr>
2023-10-23 17:19:59 +0200

    Norman té relou

--- src/slots.c
+++ src/slots.c
@@ -1,5 +1,5 @@
 #include "slots.h"
 #include <stdlib.h> // pour rand() et srand()
 #include <time.h> // pour time()
- touchez pas à mon code bande de salopards
- ici c mon territoire
+ //touchez pas à mon code bande de salopards
+ //ici c mon territoire
