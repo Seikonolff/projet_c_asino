eac0119ffb4fbf129fdc4128ab2d08be3322e4fa
7skyboom <lucca.lagouarde@gmail.com>
2023-11-26 21:13:54 +0100

    fix/V1 done, tested perso, full OK , test plz

--- include/roulette.h
+++ include/roulette.h
@@ -2,5 +2,17 @@
 #define ROULETTE_H
 
 float roulette_game(float credits);
+int userbet(int balance);
+int getBetType();
+int getSpecificNumber();
+int getColor(int betType);
+int getTier();
+void getChevalNumbers(int *num1, int *num2);
+void getCarreNumbers(int *num1, int *num2, int *num3, int *num4);
+int getColumn();
+int getTwoColumns();
+int getLine();
+int getTwoLines();
+int getNumberColor(int number);
 
 #endif //ROULETTE_H
