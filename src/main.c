c9452e5e7296262a8f76af26743cda261ddc4760
Gitzonfire <alexatz@free.fr>
2023-12-06 14:36:00 +0100

    oublie

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "casino.h"
-//#include "windows.h"
+#include "windows.h"
 
 int main() {
 
     int gameRunning = 1;
-    //SetConsoleOutputCP(65001);
+    SetConsoleOutputCP(65001);
     initialize_casino();
     while (gameRunning)
         gameRunning = lobby();
